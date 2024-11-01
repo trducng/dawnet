722c434a3fda5f210c5dd0dcd628f2e6b312d597
trducng <trungduc1992@gmail.com>
2024-11-01 13:43:20 +0700

    Migrate CacheOutput op

--- dawnet/inspector.py
+++ dawnet/inspector.py
@@ -42,8 +42,8 @@ class Handler:
 
     def forward_pre(self, module, args, kwargs):
         args, kwargs = args, kwargs
-        if self._name in self._inspector._ops:
-            for op in reversed(self._inspector._ops[self._name]):
+        if self._name in self._inspector._module_to_op:
+            for op in reversed(self._inspector._module_to_op[self._name]):
                 if op.enabled:
                     args, kwargs = op.forward_pre(
                         self._inspector, self._name, module, args, kwargs
@@ -52,8 +52,8 @@ class Handler:
 
     def forward(self, module, args, kwargs, output):
         output = output
-        if self._name in self._inspector._ops:
-            for op in self._inspector._ops[self._name]:
+        if self._name in self._inspector._module_to_op:
+            for op in self._inspector._module_to_op[self._name]:
                 if op.enabled:
                     output = op.forward(
                         self._inspector, self._name, module, args, kwargs, output
@@ -62,8 +62,8 @@ class Handler:
 
     def backward(self, module, grad_input, grad_output):
         grad_input = grad_input
-        if self._name in self._inspector._ops:
-            for op in self._inspector._ops[self._name]:
+        if self._name in self._inspector._module_to_op:
+            for op in self._inspector._module_to_op[self._name]:
                 if op.enabled:
                     grad_input = op.backward(
                         self._inspector, self._name, module, grad_input, grad_output
@@ -72,8 +72,8 @@ class Handler:
 
     def backward_pre(self, module, grad_output):
         grad_output = grad_output
-        if self._name in self._inspector._ops:
-            for op in self._inspector._ops[self._name]:
+        if self._name in self._inspector._module_to_op:
+            for op in self._inspector._module_to_op[self._name]:
                 if op.enabled:
                     grad_output = op.backward_pre(
                         self._inspector, self._name, module, grad_output
@@ -105,18 +105,26 @@ class Op:
 
 class CacheOutputOp(Op):
     def forward(self, inspector: "Inspector", name: str, module, args, kwargs, output):
-        inspector.state[name] = output
+        inspector.state.output[name] = output
         return output
 
 
-class RunState(nn.Module):
+class RunState:
+    def __init__(self):
+        self.output = {}
+        self.input = {}
+
     def clear(self):
         pass
+
+    def __getitem__(self, item, default=None):
+        return getattr(self, item)
 
 
 def copy_model(module: nn.Module) -> nn.Module:
     """Deep cop a model but shallow copy the parameters to not blow up the memory"""
     from copy import deepcopy, _deepcopy_dispatch, _deepcopy_atomic
+
     _deepcopy_dispatch[torch.Tensor] = _deepcopy_atomic
     _deepcopy_dispatch[nn.Parameter] = _deepcopy_atomic
     new_module = deepcopy(module)
@@ -131,8 +139,8 @@ class Inspector(nn.Module):
     Attributes:
         _original_model: the supplied model
         _model: the shallow cloned original model will be used to inspect
-        _ops: mapping from module name to list of `op` objects
-        _id_to_op: mapping from op id str to `op` object
+        _module_to_op: mapping from module name to list of `op` objects
+        _ops: contain ops information: op_id -> (op, module_name)
         _state: contain information that the op populates (e.g. this is where
         state: contains pulbic information that the op populates
     """
@@ -146,8 +154,8 @@ class Inspector(nn.Module):
         self._original_model = model
         self._model = copy_model(model)
 
-        self._ops: dict[str, list[Op]] = OrderedDict()
-        self._id_to_op_info: dict[str, tuple[Op, str]] = OrderedDict()
+        self._module_to_op: dict[str, list[Op]] = OrderedDict()
+        self._ops: dict[str, tuple[Op, str]] = OrderedDict()
 
         self.state: RunState = state or RunState()
         self._state = {}
@@ -160,31 +168,58 @@ class Inspector(nn.Module):
                 Handler(name, "forward", self), with_kwargs=True, always_call=True
             )
 
-    def add_op(self, name: str, op: Op):
+    def add_op(self, name: str, op: Op) -> str:
+        """Add op to the inspector
+
+        Returns:
+            the op id
+        """
         if name == "." or self._model.get_submodule(name):
-            if name not in self._ops:
-                self._ops[name] = []
-            self._ops[name].append(op)
-            self._id_to_op_info[op.id] = (op, name)
+            if name not in self._module_to_op:
+                self._module_to_op[name] = []
+            self._module_to_op[name].append(op)
+            self._ops[op.id] = (op, name)
+            return op.id
         else:
             raise ValueError(f"Module with name {name} doesn't exist")
 
+    def list_ops(self, ids: str | list[str] | None = None, name: str | None = None):
+        ops = {}
+        if ids is not None:
+            if isinstance(ids, str):
+                ids = [ids]
+            ops.update({op_id: self._ops[op_id][0] for op_id in ids})
+
+        if name is not None:
+            if name in self._module_to_op:
+                ops.update({op.id: op for op in self._module_to_op[name]})
+
+        if ids is None and name is None:
+            ops.update({op_id: op for op_id, (op, _) in self._ops.items()})
+
+        return ops
+
+    def get_op(self, id: str):
+        if id not in self._ops:
+            raise ValueError(f"Op with id {id} doesn't exist")
+        return self._ops[id][0]
+
     def remove_op(self, op_id: str):
-        if op_id not in self._id_to_op_info:
+        if op_id not in self._ops:
             raise ValueError(f"Op with id {op_id} doesn't exist")
-        layer_name = self._id_to_op_info[op_id][1]
-        self._ops[layer_name] = [op for op in self._ops[layer_name] if op.id != op_id]
-        del self._id_to_op_info[op_id]
+        layer_name = self._ops[op_id][1]
+        self._module_to_op[layer_name] = [op for op in self._module_to_op[layer_name] if op.id != op_id]
+        del self._ops[op_id]
 
     def enable_op(self, op_id: str):
-        if op_id not in self._id_to_op_info:
+        if op_id not in self._ops:
             raise ValueError(f"Op with id {op_id} doesn't exist")
-        self._id_to_op_info[op_id][0].enabled = True
+        self._ops[op_id][0].enabled = True
 
     def disable_op(self, op_id: str):
-        if op_id not in self._id_to_op_info:
+        if op_id not in self._ops:
             raise ValueError(f"Op with id {op_id} doesn't exist")
-        self._id_to_op_info[op_id][0].enabled = False
+        self._ops[op_id][0].enabled = False
 
     def copy(self) -> "Inspector":
         """Create a copy of the inspector"""
@@ -204,7 +239,7 @@ class Inspector(nn.Module):
         """Save the runner session"""
         state = {
             "model": self._model,
-            "ops": {k: v.clone() for k, v in self._ops.items()},
+            "ops": {k: v.clone() for k, v in self._module_to_op.items()},
             "hooks": self._hooks,
             "ctx": self._ctx,
             "input": self._input,
@@ -214,7 +249,7 @@ class Inspector(nn.Module):
 
     def __setstate__(self, state: dict):
         self._model = state["model"]
-        self._ops: dict[str, list[Op]] = OrderedDict()
+        self._module_to_op: dict[str, list[Op]] = OrderedDict()
 
         self._hooks = OrderedDict()
         self._ctx = OrderedDict()
@@ -224,7 +259,7 @@ class Inspector(nn.Module):
 
         for k, v in state["ops"].items():
             v.apply(self)
-            self._ops[k] = v
+            self._module_to_op[k] = v
 
         self._hooks = OrderedDict(state["hooks"])
         self._ctx = OrderedDict(state["ctx"])
@@ -249,9 +284,6 @@ class Inspector(nn.Module):
     def get_handlers(self):
         """Get handlers"""
         return [Handler(hook_id, self) for hook_id in self._hooks.keys()]
-
-    def run(self, *args, **kwargs):
-        pass
 
     def forward(self, *args, **kwargs):
         return self._model(*args, **kwargs)
