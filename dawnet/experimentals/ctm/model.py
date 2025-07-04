839ee56f40ebc7ec34e000cb91db54ac76358753
trducng <trungduc1992@gmail.com>
2025-07-04 10:10:12 +0700

    Make tick configurable

--- dawnet/experimentals/ctm/model.py
+++ dawnet/experimentals/ctm/model.py
@@ -7,7 +7,7 @@ from torchvision import datasets, transforms
 
 class Model(nn.Module):
 
-  def __init__(self, memory):
+  def __init__(self, memory, nticks):
     super().__init__()
     self.feature_extractor = nn.Conv2d(
       in_channels=1, out_channels=64, kernel_size=2, stride=1, padding=1,
@@ -28,6 +28,7 @@ class Model(nn.Module):
     self.register_parameter(
       "b", nn.Parameter(torch.empty(1).normal_(), requires_grad=True),
     )
+    self.nticks = nticks
 
   def forward(self, x):
     bs = x.size(0)
@@ -39,7 +40,7 @@ class Model(nn.Module):
     extracted_feature = f.relu(self.linear1(extracted_feature).squeeze(-1)) # B,H*W
     active_state = self.active_state.unsqueeze(0).expand(bs, -1)  # B,M
 
-    for tick_idx in range(10):
+    for tick_idx in range(self.nticks):
       post_act = torch.concat([extracted_feature, active_state], dim=1) # B,H*W+M
       pre_act = f.relu(self.linear2(post_act).unsqueeze(-1)) # B,512,1
       # print(f"== {tick_idx=}")
@@ -81,7 +82,7 @@ if __name__ == "__main__":
   images = torch.stack(images)
 
   print(f"{images.shape=}")
-  model = Model(memory=15,)
+  model = Model(memory=15, nticks=10)
   model.train()
   init_mem = model.memory.clone().detach()
   model = model.to(device=device)
@@ -89,7 +90,7 @@ if __name__ == "__main__":
   loss_obj = nn.CrossEntropyLoss()
   optimizer = optim.Adam(params=model.parameters())
   trainloader = torch.utils.data.DataLoader(train_data, batch_size=256, shuffle=True, num_workers=1)
-  testloader = torch.utils.data.DataLoader(test_data, batch_size=256, shuffle=True, num_workers=1, drop_last=False)
+  testloader = torch.utils.data.DataLoader(test_data, batch_size=256, shuffle=False, num_workers=1, drop_last=False)
   count = 0
   for x, y in trainloader:
     x, y = x.to(device), y.to(device)
@@ -105,17 +106,69 @@ if __name__ == "__main__":
   correct = 0
   total = 0
   model.eval()
+  track = None
+  before = model.active_state.clone().detach()
+  before_memory = model.memory.clone().detach()
+  before_w = model.w.clone().detach()
+  before_b = model.b.clone().detach()
+  run1_inputs, run1_labels, run1_outputs, run1_preds = [], [], [], []
   with torch.no_grad():
-    for images, labels in testloader:
+    for _i1, (images, labels) in enumerate(testloader):
+      if _i1 == 0:
+        track = (images, labels)
       images, labels = images.to(device), labels.to(device)
       outputs = model(images)
       _, predicted = torch.max(outputs.data, 1)
+      run1_inputs.append(images)
+      run1_labels.append(labels)
+      run1_outputs.append(outputs)
+      run1_preds.append(predicted)
       total += labels.size(0)
       correct += (predicted == labels).sum().item()
 
+  # run1_inputs = [each for sublist in run1_inputs for each in sublist]
+  # run1_labels = [each for sublist in run1_labels for each in sublist]
+  # run1_outputs = [each for sublist in run1_outputs for each in sublist]
+  # run1_preds = [each for sublist in run1_preds for each in sublist]
+
   accuracy = 100 * correct / total
+  print(f"{correct=}, {total=}")
   print(f"Accuracy: {accuracy:.2f}%")
   post_mem = model.memory.clone().detach().cpu()
   print((post_mem - init_mem).sum())
+
+  track2 = None
+  run2_inputs, run2_labels, run2_outputs, run2_preds = [], [], [], []
+  for _ntick in range(20):
+    correct, total = 0, 0
+    model.nticks = _ntick
+    with torch.no_grad():
+      for _i1, (images, labels) in enumerate(testloader):
+        if _i1 == 0:
+          track2 = (images, labels)
+        images, labels = images.to(device), labels.to(device)
+        outputs = model(images)
+        _, predicted = torch.max(outputs.data, 1)
+        run2_inputs.append(images)
+        run2_labels.append(labels)
+        run2_outputs.append(outputs)
+        run2_preds.append(predicted)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+    accuracy = 100 * correct / total
+    print(f"{correct=}, {total=}")
+    print(f"{_ntick=} Accuracy: {accuracy:.2f}%")
+  after = model.active_state.clone().detach()
+  after_memory = model.memory.clone().detach()
+  after_w = model.w.clone().detach()
+  after_b = model.b.clone().detach()
+
+  # run2_inputs = [each for sublist in run2_inputs for each in sublist]
+  # run2_labels = [each for sublist in run2_labels for each in sublist]
+  # run2_outputs = [each for sublist in run2_outputs for each in sublist]
+  # run2_preds = [each for sublist in run2_preds for each in sublist]
+
+  import numpy as np
 
 # vim: ts=2 sts=2 sw=2 et
