52720f1d27c72f338214cbcff1fafba18d6d566e
trducng <trungduc1992@gmail.com>
2025-07-12 08:48:54 +0700

    training

--- dawnet/experimentals/ctm/model_reimplement.py
+++ dawnet/experimentals/ctm/model_reimplement.py
@@ -25,7 +25,7 @@ class Sync(nn.Module):
 
   def forward(self, post_act_hist):
     B, M, T = post_act_hist.shape   # batch, n_neurons, time
-    decay = torch.arange(start=T, end=0, step=-1)
+    decay = torch.arange(start=T, end=0, step=-1, device=self._decay.device)
     decay = self._decay ** decay
     decay = decay.unsqueeze(0).unsqueeze(0).expand(B, 1, T)
 
@@ -212,16 +212,144 @@ class CTM(nn.Module):
 
     return outputs
 
-
 if __name__ == "__main__":
-  feat_shape = 64
-  chosen = 256
-  B, S, C = 8, 24, feat_shape
+  import random
+  import torch.optim as optim
+  from torchvision import datasets, transforms
+  device = torch.device("mps")
+  transform = transforms.Compose(
+    [
+      # transforms.RandomAffine(degrees=15, translate=(0.1, 0.1), scale=(0.9, 1.1)),
+      # transforms.RandomRotation(10),
+      transforms.ToTensor(),
+      transforms.Normalize((0.1307,), (0.3081,)),
+    ]
+  )
+  train_data = datasets.MNIST(
+    root="/Users/john/dawnet/temp/data", train=True, download=True, transform=transform
+  )
+  test_data = datasets.MNIST(
+    root="/Users/john/dawnet/temp/data", train=False, download=True, transform=transform
+  )
 
-  attn = VanillaAttention(feat_shape=feat_shape)
-  rep_ = torch.Tensor(size=(B, chosen)).uniform_()
-  feat_ = torch.Tensor(size=(B, S, C)).uniform_()
-  o_ = attn(rep_, feat_)
-  print(o_.shape)
+  images = []
+  for i in range(5):
+    image, _ = train_data[i]
+    images.append(image)
+
+  images = torch.stack(images)
+
+  print(f"{images.shape=}")
+  model = CTM(size=512, memory=15, nticks=10, nout=10)
+  model.train()
+  # init_mem = model.memory.clone().detach()
+  model = model.to(device=device)
+
+  loss_obj = nn.CrossEntropyLoss()
+  optimizer = optim.Adam(params=model.parameters())
+  trainloader = torch.utils.data.DataLoader(
+    train_data, batch_size=256, shuffle=True, num_workers=1
+  )
+  testloader = torch.utils.data.DataLoader(
+    test_data, batch_size=256, shuffle=False, num_workers=1, drop_last=False
+  )
+  count = 0
+  for x, y in trainloader:
+    x = x.expand(-1,3,-1,-1)
+    x, y = x.to(device), y.to(device)
+    count += 1
+    out = model(x)
+
+    out_idx = random.randint(0, len(out)-1)
+    loss = loss_obj(out[out_idx], y)
+
+    # for idx, each in enumerate(out):
+    #   if idx == 0:
+    #     loss = loss_obj(each, y)
+    #   else:
+    #     loss += loss_obj(each, y)
+    loss.backward()
+    optimizer.step()
+    optimizer.zero_grad()
+    if count % 100 == 0:
+      print(f"{count=}, {loss.item()=}")
+
+  correct = 0
+  total = 0
+  model.eval()
+  track = None
+  # before = model.active_state.clone().detach()
+  # before_memory = model.memory.clone().detach()
+  # before_w = model.w.clone().detach()
+  # before_b = model.b.clone().detach()
+  run1_inputs, run1_labels, run1_outputs, run1_preds = [], [], [], []
+  with torch.no_grad():
+    for _i1, (images, labels) in enumerate(testloader):
+      images = images.expand(-1,3,-1,-1)
+      if _i1 == 0:
+        track = (images, labels)
+      images, labels = images.to(device), labels.to(device)
+      out = model(images)
+
+      out_idx = random.randint(0, len(out)-1)
+      outputs = out[out_idx]
+
+      # outputs = model(images)[-1]
+      _, predicted = torch.max(outputs.data, 1)
+      run1_inputs.append(images)
+      run1_labels.append(labels)
+      run1_outputs.append(outputs)
+      run1_preds.append(predicted)
+      total += labels.size(0)
+      correct += (predicted == labels).sum().item()
+
+  # run1_inputs = [each for sublist in run1_inputs for each in sublist]
+  # run1_labels = [each for sublist in run1_labels for each in sublist]
+  # run1_outputs = [each for sublist in run1_outputs for each in sublist]
+  # run1_preds = [each for sublist in run1_preds for each in sublist]
+
+  accuracy = 100 * correct / total
+  # print(f"{correct=}, {total=}")
+  print(f"Accuracy: {accuracy:.2f}%")
+  # post_mem = model.memory.clone().detach().cpu()
+  # print((post_mem - init_mem).sum())
+
+  track2 = None
+  run2_inputs, run2_labels, run2_outputs, run2_preds = [], [], [], []
+  for _ntick in range(1, 20):
+    correct, total = 0, 0
+    model._nticks = _ntick
+    with torch.no_grad():
+      for _i1, (images, labels) in enumerate(testloader):
+        images = images.expand(-1,3,-1,-1)
+        if _i1 == 0:
+          track2 = (images, labels)
+        images, labels = images.to(device), labels.to(device)
+        out = model(images)
+
+        out_idx = random.randint(0, len(out)-1)
+        outputs = out[out_idx]
+
+        # outputs = out[-1]
+        _, predicted = torch.max(outputs.data, 1)
+        run2_inputs.append(images)
+        run2_labels.append(labels)
+        run2_outputs.append(outputs)
+        run2_preds.append(predicted)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+    accuracy = 100 * correct / total
+    # print(f"{correct=}, {total=}")
+    print(f"{_ntick=} Accuracy: {accuracy:.2f}%")
+  # after = model.active_state.clone().detach()
+  # after_memory = model.memory.clone().detach()
+  # after_w = model.w.clone().detach()
+  # after_b = model.b.clone().detach()
+
+  # run2_inputs = [each for sublist in run2_inputs for each in sublist]
+  # run2_labels = [each for sublist in run2_labels for each in sublist]
+  # run2_outputs = [each for sublist in run2_outputs for each in sublist]
+  # run2_preds = [each for sublist in run2_preds for each in sublist]
 
 # vim: ts=2 sts=2 sw=2 et
